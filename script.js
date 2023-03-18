06b2642c34a5253a0bd79921e849a00d45eb7055
Arinze <calistusarinzechinweuba@gmail.com>
2023-03-18 09:12:36 +0100

    Solve the problem of if the input field is an empty one

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ submitToCheck.addEventListener("click", (event) => {
   const remainder = val % 2;
   const even = "Hey, this number is even bro";
   const odd = "Hey, this number is odd bro";
-  const insert = "You need to insert";
+  const insert = "You need to insert a number bro!";
   const emptyField = "";
+  const strg = "This is not a number men!";
 
   if (remainder == 0) {
     document.getElementById("demo").innerHTML = even;
   }else {
     document.getElementById("demo").innerHTML = odd;
+  }if(val === emptyField){
+     document.getElementById("demo").innerHTML = insert;
   }
 });
 

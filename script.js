3a41d19c0a2a3ba1b0b8dffcea39f53353e9de80
Arinze <calistusarinzechinweuba@gmail.com>
2023-03-18 04:47:15 +0100

    Changed Project

---
+++ script.js
@@ -0,0 +1,19 @@
+const userInput = document.querySelector("#fname");
+const submitToCheck = document.querySelector(".btn");
+
+submitToCheck.addEventListener("click", (event) => {
+  event.preventDefault(); // prevent the default form submission behavior
+
+  const val = userInput.value;
+  const remainder = val % 2;
+  const even = "Hey, this number is even bro";
+  const odd = "Hey, this number is odd bro";
+  const insert = "You need to insert";
+  const emptyField = "";
+
+  if (remainder == 0) {
+    document.getElementById("demo").innerHTML = even;
+  }else {
+    document.getElementById("demo").innerHTML = odd;
+  }
+});
